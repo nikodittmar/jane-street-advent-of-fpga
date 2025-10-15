5edd65eec49283314129ba47597f768172fb4468
slapblackjack <jstnhoangg@gmail.com>
2025-10-14 17:16:17 -0700

    feat: added ex control logic

--- hardware/src/riscv_core/control/ex_control.v
+++ hardware/src/riscv_core/control/ex_control.v
@@ -1,5 +1,5 @@
-`include "alu_sel.vh"
-`include "opcode.vh"
+`include "control_sel.vh"
+`include "../opcode.vh"
 
 module ex_control (
     input [31:0] inst,
@@ -18,105 +18,268 @@ assign opcode5 = inst[6:2];
 assign funct3 = inst[14:12];
 assign funct7 = inst[31:25];
 
-case (opcode5)
-`OPC_LUI_5:
-    // LUI
-`OPC_AUIPC_5:
-    // AUIPC
-`OPC_JAL_5:
-    // JAL
-`OPC_JALR_5:
-    // JALR
-`OPC_BRANCH_5:
-    case (funct3)
-    `FNC_BEQ:
-        // BEQ
-    `FNC_BNE:
-        // BNE
-    `FNC_BLT:
-        // BLT
-    `FNC_BGE:
-        // BGE
-    `FNC_BLTU:
-        // BLTU
-    `FNC_BGEU:
-        // BGEU
-    endcase
-`OPC_STORE_5:
-    case (funct3)
-    `FNC_SB:
-        // SB
-    `FNC_SH:
-        // SH
-    `FNC_SW:
-        // SW
-    endcase
-`OPC_LOAD_5:
-    case (funct3)
-    `FNC_LB:
-        // LB
-    `FNC_LH:
-        // LH
-    `FNC_LW:
-        // LW
-    `FNC_LBU:
-        // LBU
-    `FNC_LHU:
-        // LHU
-    endcase
-`OPC_ARI_RTYPE_5:
-    case (funct3)
-    `FNC_ADD_SUB:
-        case (inst[30])
-        `FNC2_ADD:
-        // ADD
-        `FNC2_SUB:
-        // SUB
+always @ (*) begin
+    case (opcode5)
+    `OPC_ARI_RTYPE_5:
+        case (funct3)
+        `FNC_ADD_SUB:
+            case (inst[30])
+            `FNC2_ADD: begin
+                // ADD
+                asel = `A_REG;
+                bsel = `B_REG;
+                alusel = `ALU_ADD;
+            end
+            `FNC2_SUB: begin
+                // SUB
+                asel = `A_REG;
+                bsel = `B_REG;
+                alusel = `ALU_SUB;
+            end
+            endcase
+        `FNC_AND: begin
+            // AND
+            asel = `A_REG;
+            bsel = `B_REG;
+            alusel = `ALU_AND;
+        end
+        `FNC_OR: begin
+            // OR
+            asel = `A_REG;
+            bsel = `B_REG;
+            alusel = `ALU_OR;
+        end
+        `FNC_XOR: begin
+            // XOR
+            asel = `A_REG;
+            bsel = `B_REG;
+            alusel = `ALU_XOR;
+        end
+        `FNC_SLL: begin
+            // SLL
+            asel = `A_REG;
+            bsel = `B_REG;
+            alusel = `ALU_SLL;
+        end
+        `FNC_SRL_SRA:
+            case (inst[30])
+            `FNC2_SRL: begin
+                // SRL
+                asel = `A_REG;
+                bsel = `B_REG;
+                alusel = `ALU_SRL;
+            end
+            `FNC2_SRA: begin
+                // SRA
+                asel = `A_REG;
+                bsel = `B_REG;
+                alusel = `ALU_SRA;
+            end
+            endcase
+        `FNC_SLT: begin
+            // SLT
+            asel = `A_REG;
+            bsel = `B_REG;
+            alusel = `ALU_SLT;
+        end
+        `FNC_SLTU: begin
+            // SLTU
+            asel = `A_REG;
+            bsel = `B_REG;
+            alusel = `ALU_SLT;
+        end
         endcase
-    `FNC_SLL:
-        // SLL
-    `FNC_SLT:
-        // SLT
-    `FNC_SLTU:
-        // SLTU
-    `FNC_XOR:
-        // XOR
-    `FNC_OR:
-        // OR
-    `FNC_AND:
-        // AND
-    `FNC_SRL_SRA:
-        case (inst[30])
-        `FNC2_SRL:
-            // SRL
-        `FNC2_SRA:
-            // SRA
+
+    `OPC_ARI_ITYPE_5:
+        case (funct3)
+        `FNC_ADD_SUB: begin
+            // ADDI
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        `FNC_SLL: begin
+            // SLLI
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_SLL;
+        end
+        `FNC_SLT: begin
+            // SLTI
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_SLT;
+            brun = 1'b0;
+        end
+        `FNC_SLTU: begin
+            // SLTIU
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_SLT;
+            brun = 1'b1;
+        end
+        `FNC_XOR: begin
+            // XORI
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_XOR;
+        end
+        `FNC_OR: begin
+            // ORI
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_OR;
+        end
+        `FNC_AND: begin
+            // ANDI
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_AND;
+        end
+        `FNC_SRL_SRA:
+            case (inst[30])
+            `FNC2_SRL: begin
+                // SRLI
+                asel = `A_REG;
+                bsel = `B_IMM;
+                alusel = `ALU_SRL;
+            end
+            `FNC2_SRA: begin
+                // SRAI
+                asel = `A_REG;
+                bsel = `B_IMM;
+                alusel = `ALU_SRA;
+            end
+            endcase
         endcase
-    endcase
-`OPC_ARI_ITYPE_5:
-    case (funct3)
-    `FNC_ADD_SUB:
-        // ADDI
-    `FNC_SLL:
-        // SLLI
-    `FNC_SLT:
-        // SLTI
-    `FNC_SLTU:
-        // SLTIU
-    `FNC_XOR:
-        // XORI
-    `FNC_OR:
-        // ORI
-    `FNC_AND:
-        // ANDI
-    `FNC_SRL_SRA:
-        case (inst[30])
-        `FNC2_SRL:
-            // SRLI
-        `FNC2_SRA:
-            // SRAI
+    
+    `OPC_LOAD_5:
+        case (funct3)
+        `FNC_LB: begin
+            // LB
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        `FNC_LH: begin
+            // LH
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        `FNC_LW: begin
+            // LW
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        `FNC_LBU: begin
+            // LBU
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        `FNC_LHU: begin
+            // LHU
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
         endcase
+
+    `OPC_STORE_5:
+        case (funct3)
+        `FNC_SB: begin
+            // SB
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        `FNC_SH: begin
+            // SH
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        `FNC_SW: begin
+            // SW
+            asel = `A_REG;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        endcase
+    
+    `OPC_BRANCH_5:
+        case (funct3)
+        `FNC_BEQ: begin
+            // BEQ
+            asel = `A_PC;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        `FNC_BNE: begin
+            // BNE
+            asel = `A_PC;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+        end
+        `FNC_BLT: begin
+            // BLT
+            asel = `A_PC;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+            brun = 1'b0;
+        end
+        `FNC_BGE: begin
+            // BGE
+            asel = `A_PC;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+            brun = 1'b0;
+        end
+        `FNC_BLTU: begin
+            // BLTU
+            asel = `A_PC;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+            brun = 1'b1;
+        end
+        `FNC_BGEU: begin
+            // BGEU
+            asel = `A_PC;
+            bsel = `B_IMM;
+            alusel = `ALU_ADD;
+            brun = 1'b1;
+        end
+        endcase
+    
+    `OPC_JAL_5: begin
+        // JAL
+        asel = `A_PC;
+        bsel = `B_IMM;
+        alusel = `ALU_ADD;
+    end
+    `OPC_JALR_5: begin
+        // JALR
+        asel = `A_PC;
+        bsel = `B_IMM;
+        alusel = `ALU_ADD;
+    end
+
+    `OPC_LUI_5: begin
+        // LUI
+        asel = `A_PC;
+        bsel = `B_IMM;
+        alusel = `ALU_BSEL;
+    end
+    `OPC_AUIPC_5: begin
+        // AUIPC
+        asel = `A_PC;
+        bsel = `B_IMM;
+        alusel = `ALU_ADD;
+    end
+    
     endcase
-endcase
+end
 
 endmodule
