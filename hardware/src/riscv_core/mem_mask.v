073c59ca31747d74a161a0b11254d30d834ad7a9
slapblackjack <jstnhoangg@gmail.com>
2025-10-18 16:34:50 -0700

    feat: added memory mask module

--- hardware/src/riscv_core/mem_mask.v
+++ hardware/src/riscv_core/mem_mask.v
@@ -1,7 +1,22 @@
 module mem_mask (
     input [31:0] din,
     input [3:0] mask,
-    output [31:0] dout
+    output reg [31:0] dout
 );
+
+// TODO: SV assertion that mask is valid
+
+always @ (*) begin
+    case(mask)
+    4'b0001: dout = {24'b0, din[7:0]};
+    4'b0010: dout = {24'b0, din[15:8]};
+    4'b0100: dout = {24'b0, din[23:16]};
+    4'b1000: dout = {24'b0, din[31:24]};
+    4'b0011: dout = {16'b0, din[15:0]};
+    4'b1100: dout = {16'b0, din[31:16]};
+    4'b1111: dout = din;
+    endcase
+
+end
 
 endmodule
