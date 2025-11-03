837d9b38cb69318c75327bc95480b5969e601527
nikodittmar <nikodittmar@gmail.com>
2025-11-03 15:42:10 -0800

    feat: finished fp reg file

--- hardware/src/riscv_core/fpu/fp_reg_file.v
+++ hardware/src/riscv_core/fpu/fp_reg_file.v
@@ -5,12 +5,14 @@ module fp_reg_file (
     input [31:0] wd,
     output [31:0] rd1, rd2, rd3
 );
+    parameter DEPTH = 32;
+
     reg [31:0] mem [0:31];
 
     // Asynchronous read
-    assign rd1 = mem[ra1];
-    assign rd2 = mem[ra2];
-    assign rd3 = mem[ra3];
+    assign rd1 = (we && wa == ra1) ? wd : mem[ra1];
+    assign rd2 = (we && wa == ra2) ? wd : mem[ra2];
+    assign rd3 = (we && wa == ra3) ? wd : mem[ra3];
 
     // Synchronous write
     always @(posedge clk) begin
